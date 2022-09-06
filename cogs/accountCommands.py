16b54ab4bd342cf7d85c3e8ff712d13d4eac51a9
BabyDinos <108362808+BabyDinos@users.noreply.github.com>
2022-09-05 20:37:05 -0700

    Delete
    Deleted messages after a certain amount of time

--- cogs/accountCommands.py
+++ cogs/accountCommands.py
@@ -18,13 +18,13 @@ class accCommands(commands.Cog):
         self.id = str(ctx.author).split('#')[-1]
         res = sqlCommands.load(self.id, unit = 'player') 
         if res:
-            await ctx.send('This User is already linked to a pre-existing account')
+            await ctx.send('This User is already linked to a pre-existing account', delete_after = 20)
         else:
             embed = discord.Embed(
                 title = 'Registering',
                 description = 'Enter your username: '
             )
-            bot_message = await ctx.send(embed = embed)
+            self.bot_message = await ctx.send(embed = embed)
             
             try:
                 self.username_message = await self.bot.wait_for('message', timeout = 20, check= lambda message: message.author == ctx.author and message.channel == ctx.channel)
@@ -39,7 +39,7 @@ class accCommands(commands.Cog):
                                         {}'''.format(baseDifference('Mage')))          
                 embed.set_footer(text = 'Choose your class: ')      
 
-                await bot_message.edit(embed = embed)
+                await self.bot_message.edit(embed = embed)
 
                 self.class_message = await self.bot.wait_for('message', timeout = 20, check= lambda message: message.author == ctx.author and message.channel == ctx.channel)
 
@@ -55,34 +55,54 @@ class accCommands(commands.Cog):
                         title = 'Thanks for Registering ' + str(self.username_message.content),
                         description = 'Welcome to RPG!'
                     )
-                    
-                    await bot_message.edit(embed = embed)
+                    await self.username_message.delete()
+                    await self.class_message.delete()
+                    await ctx.message.delete()
+                    await self.bot_message.edit(embed = embed)
             except asyncio.TimeoutError: 
-                await ctx.send('Command Timedout')
-
+                await ctx.send('Command Timedout', delete_after = 20)
+                asyncio.sleep(20)
+                try:
+                    await self.username_message.delete()
+                except:
+                    pass
+                try: 
+                    await self.class_message.delete()
+                except:
+                    pass
+                await self.bot_message.delete()
+                await ctx.message.delete()
+                
     @commands.command()
     async def nameChange(self, ctx):
         self.id = str(ctx.author).split('#')[-1]
         res = sqlCommands.load(self.id, unit = 'player')
         if not res:
-            await ctx.send('You are not registered')
+            await ctx.send('You are not registered', delete_after = 20)
         else:
-            await ctx.send('What is your new Username?')
+            await ctx.send('What is your new Username?', delete_after = 20)
             try:
                 self.message = await self.bot.wait_for('message', timeout = 20, check= lambda message: message.author == ctx.author and message.channel == ctx.channel)
                 if self.message:
                     res.Name = str(self.message.content)
                     sqlCommands.save(self.id, res, unit = 'player')
-                    await ctx.send('Your name has been changed to ' + str(self.message.content))
+                    await ctx.send('Your name has been changed to ' + str(self.message.content), delete_after = 20)
+                await self.message.delete()
+                await ctx.message.delete()
             except asyncio.TimeoutError: 
-                await ctx.send('Command Timedout')
+                await ctx.send('Command Timedout', delete_after = 20)
+                try:
+                    await self.message.delete()
+                except:
+                    pass
+        await ctx.message.delete()
 
     @commands.command()
     async def info(self, ctx):
         self.id = str(ctx.author).split('#')[-1]
         res = sqlCommands.load(self.id, unit = 'player')
         if not res:
-            await ctx.send('You are not registered')
+            await ctx.send('You are not registered', delete_after = 20)
         else:
             playerinfo = playerInfo(res)
             embed = discord.Embed(
@@ -91,16 +111,17 @@ class accCommands(commands.Cog):
             )
             embed.add_field(name = 'Infos', value = playerinfo[0])
             embed.add_field(name = 'Equipment', value = playerinfo[1])
-            await ctx.send(embed = embed)
+            await ctx.send(embed = embed, delete_after = 120)
+        await ctx.message.delete()
 
     @commands.command()
     async def delete(self, ctx):
         self.id = str(ctx.author).split('#')[-1]
         res = sqlCommands.load(self.id, unit = 'player')
         if not res:
-            await ctx.send('You are not registered')
+            await ctx.send('You are not registered', delete_after = 20)
         else:
-            msg = await ctx.send('Are you sure you want to delete? - ' + res.Name)
+            msg = await ctx.send('Are you sure you want to delete? - ' + res.Name, delete_after = 20)
             await msg.add_reaction("\U00002705")
             await msg.add_reaction("\U0000274C")
 
@@ -110,21 +131,26 @@ class accCommands(commands.Cog):
             try:
                 reaction, user = await self.bot.wait_for('reaction_add', timeout = 20, check= check)
             except asyncio.TimeoutError:
-                await ctx.send('Command Timedout')
+                try:
+                    await ctx.message.delete()
+                except:
+                    pass
+                await ctx.send('Command Timedout', delete_after = 20)
                 return
             else:
                 if str(reaction.emoji) == "\U00002705":
                     sqlCommands.delete(self.id, unit = 'player')
-                    await ctx.send('Account has been deleted')
+                    await ctx.send('Account has been deleted', delete_after = 20)
                 else:
-                    await ctx.send('Deletion Cancelled')
+                    await ctx.send('Deletion Cancelled', delete_after = 20)
+        await ctx.message.delete()
 
     @commands.command()
     async def inventory(self, ctx):
         self.id = str(ctx.author).split('#')[-1]
         res = sqlCommands.load(self.id, unit = 'player')
         if not res:
-            await ctx.send('You are not registered')
+            await ctx.send('You are not registered', delete_after = 20)
         else:
             playerinv = playerInventory(res)
             length = len(playerinv)
@@ -134,9 +160,8 @@ class accCommands(commands.Cog):
             )
             for x in range(0,length,2):
                 embed.add_field(name = playerinv[x], value = playerinv[x+1])
-            await ctx.send(embed = embed)
-        pass
-
+            await ctx.send(embed = embed, delete_after = 120)
+        await ctx.message.delete()
 
 async def setup(bot):
     await bot.add_cog(accCommands(bot))
