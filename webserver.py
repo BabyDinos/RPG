ef2e16b3da02c4137782062f0925c02303dcc255
BabyDinos <108362808+BabyDinos@users.noreply.github.com>
2022-09-11 19:53:44 +0000

    Fixed .replit file

--- webserver.py
+++ webserver.py
@@ -10,8 +10,8 @@ def home():
 
     return "I'm alive"
 def run():
-
-  app.run(host='0.0.0.0',port=8080)
+  from waitress import serve
+  serve(app, host='0.0.0.0',port=8080)
 
 def keep_alive():  
 
