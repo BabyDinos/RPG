a220055fa890236a60b93b0090986da31fc2b728
BabyDinos <108362808+BabyDinos@users.noreply.github.com>
2022-09-15 22:31:30 -0700

    Premium Lootbox

--- lootbox.py
+++ lootbox.py
@@ -20,3 +20,8 @@ class CommonLootbox(lootbox):
         self.items = ['Gold','Meat','Hide','Bark','Stone','Stone Sword', 'Stone Staff', 'Hide Armor','Hide Robe','Bark Armor','Bark Robe','Bark Axe','Bark Wand','Panther Cub','Golemite','Twewant']
         self.probabilities = [19,19,19,19,19,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.33,0.33,0.33]
 
+class PremiumLootBox(lootbox):
+
+    def __init__(self):
+        self.items = ['Gold','Gem','Panther Tooth','Golden Apple','Tooth Spear', 'Tooth Scepter', 'Gem Armor','Gem Robe','Golden Armor','Golden Robe','Golden Daggers','Golden Staff','Quadruple Fanged Panther','Gem Golem','Golden Treant']
+        self.probabilities = [23.75,23.75,23.75,23.75,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.33,0.33,0.33]
