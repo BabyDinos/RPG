7da5f89712d29b54d69917a07c86fb615911bd9f
BabyDinos <108362808+BabyDinos@users.noreply.github.com>
2022-09-15 17:18:01 -0700

    player update

--- adminUpdate.py
+++ adminUpdate.py
@@ -7,7 +7,11 @@ with SqliteDict('player.sqlite') as mydict:
     newplayer = playerClass.Warrior(player.Name)
     for variable in vars(player).keys():
         if variable in ['CurrentLevel','MaxLevel','Level']:
-            newplayer[variable] = vars(player)[variable]
+            vars(newplayer)[variable] = vars(player)[variable]
+        elif variable in ['statpoints','totalstatpoints']:
+            vars(newplayer)[variable] = 90
+        elif variable == 'stats_dictionary':
+            vars(newplayer)[variable] = {'Max Health' : 10, 'Attack':5,'Magic Attack':5,'Defense':5,'Magic Defense': 5, 'Attack Speed':1}
         else:
             vars(newplayer)[variable] = vars(player)[variable]
     print(vars(newplayer))
